83cd5cac0f407d8a78ca8846d8de4c2a0c0bfb77
wpiech <43981628+wpiech@users.noreply.github.com>
2019-11-03 19:51:20 +0100

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,30 @@ namespace apka
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {"[", "[",".", ".", "T", "T", "I", "I",
+            "Q", "Q", "P", "P", "6", "6", "A", "A"
+        };
+        Label firstclicked, secondclicked;
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        { Label label;
+            int randomnumber;
+            for(int i = 0; i<tableLayoutPanel1.Controls.Count; i++)
+            { if (tableLayoutPanel1.Controls[i] is Label)
+                    label = (Label)tableLayoutPanel1.Controls[i];
+                else
+                    continue;
+                randomnumber = random.Next(0, icons.Count);
+                label.Text = icons[randomnumber];
+                icons.RemoveAt(randomnumber);
+            }
         }
     }
 }
